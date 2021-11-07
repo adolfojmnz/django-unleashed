dfdfd4ff9461b9c8ea6a04e9842c734d9d608a73
Eadwulf <eadwulfts@protonmail.com>
2021-11-07 09:34:55 -0400

    tag_list and tag_detail templates created

--- organizer/views.py
+++ organizer/views.py
@@ -1,24 +1,9 @@
-from django.http.response import HttpResponse
+from django.shorcuts import render
 
 from .models import Tag, Startup, NewsLink
 
 
 def homepage(request):
-    li = ''
-
-    for tag in Tag.objects.all():
-        li += f'<li>{tag.__str__()}</li>\n'
-
-    output = (
-        '<!DOCTYPE HTML>' +
-        '<html>' +
-        ' <head>' +
-        '   <Title>HomePage</title>' +
-        '  </head>' +
-        '  <body>' +
-        '    <h3>Tags</h3>' +
-        f'    {li}' +
-        '  </body>' +
-        '</hml>'
-        )
-    return HttpResponse(output)
+    output = ''
+    template_name = 'templates/organizer/tag_detail.html'
+    return render(output, template_name)
