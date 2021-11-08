f7a4284be14b5b50118deea39ac64a97dd0b8ff8
Eadwulf <eadwulfts@protonmail.com>
2021-11-07 22:42:24 -0400

    more of DTL

--- organizer/views.py
+++ organizer/views.py
@@ -1,9 +1,18 @@
-from django.shorcuts import render
-
+from django.shortcuts import render
 from .models import Tag, Startup, NewsLink
 
-
 def homepage(request):
-    output = ''
-    template_name = 'templates/organizer/tag_detail.html'
-    return render(output, template_name)
+    template_name = 'organizer/homepage.html'
+    return render(request, template_name)
+
+def tag_list(request):
+    template_name = 'organizer/tag_list.html'
+    return render(request, template_name)
+
+def tag_detail(request):
+    template_name = 'organizer/tag_detail.html'
+    return render(request, template_name)
+
+def startup_detail(request):
+    template_name = 'organizer/startup_detail.html'
+    return render(request, template_name)
