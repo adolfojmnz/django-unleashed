c3d7deba8de923b8dbb9fe8b83e8574be0070a2e
Eadwulf <eadwulfts@protonmail.com>
2021-11-14 16:43:43 -0400

    add relation between TagForm and Tag

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,17 +1,14 @@
 from django.core.exceptions import ValidationError
 from django import forms
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(max_length=31,
-                  help_text='A label for URL config.')
+from .models import Tag
 
-    def save(self):
-        new_tag = Tag.objects.create(
-                name=self.cleaned_data['name'],
-                slug=self.cleaned_data['slug'],
-        )
-        return new_tag
+
+class TagForm(forms.ModelForm):
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
     def clean_name(self):
         return self.clean_data['name'].lower()
