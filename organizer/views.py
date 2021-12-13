d74b9a4cc64798ad2b08465e733aa1ae7424fb59
Eadwulf <eadwulfts@protonmail.com>
2021-12-13 10:23:54 -0400

    Add tag_create view to create new tags

--- organizer/views.py
+++ organizer/views.py
@@ -13,9 +13,12 @@ def tag_list(request):
     return render(request, template_name, context)
 
 def tag_detail(request, slug):
-    template_name = 'organizer/tag_detail.html'
-    context = {'tag': get_object_or_404(Tag, slug__iexact=slug)}
-    return render(request, template_name, context)
+	if slug == 'create':
+		return tag_create(request)
+	else:
+	    template_name = 'organizer/tag_detail.html'
+	    context = {'tag': get_object_or_404(Tag, slug__iexact=slug)}
+	    return render(request, template_name, context)
 
 def startup_list(request):
     template_name = 'organizer/startup_list.html'
@@ -27,3 +30,7 @@ def startup_detail(request, slug):
     context = {'startup': get_object_or_404(Startup, slug__iexact=slug)}
     return render(request, template_name, context)
 
+# helper functions
+def tag_create(request):
+	template_name = 'organizer/tag_form.html'
+	return render(request, template_name)
