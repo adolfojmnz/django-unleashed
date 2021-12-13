ee6c0611d1d2d5d896a8f0044e711b613ada7eea
Eadwulf <eadwulfts@protonmail.com>
2021-12-13 14:17:24 -0400

    Add urls for tag_create and tag_update views

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import (
-        homepage, tag_list, tag_detail, tag_create, startup_list, startup_detail
+        homepage,
+		tag_list, tag_detail, tag_create, tag_update,
+		startup_list, startup_detail
 )
 
 
@@ -10,6 +12,7 @@ urlpatterns = [
         path('tag/', tag_list, name='tag_list'),
         path('tag/<slug:slug>/', tag_detail, name='tag_detail'),
 		path('tag/create/', tag_create, name='tag_create'),
+		path('tag/<slug:slug>/update/', tag_update, name='tag_update'),
 
         # startups urls
         path('startup/', startup_list, name='startup_list'),
