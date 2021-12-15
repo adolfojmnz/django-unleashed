4f3c30863fbbea59b1cc43d36c44d14d471e7587
Eadwulf <eadwulfts@protonmail.com>
2021-12-15 11:18:03 -0400

    Put tag_create before tag_detail to set the correct precedence

--- organizer/urls.py
+++ organizer/urls.py
@@ -10,8 +10,8 @@ from .views import (
 urlpatterns = [
         # tags urls
         path('tag/', tag_list, name='tag_list'),
-        path('tag/<slug:slug>/', tag_detail, name='tag_detail'),
 		path('tag/create/', tag_create, name='tag_create'),
+        path('tag/<slug:slug>/', tag_detail, name='tag_detail'),
 		path('tag/<slug:slug>/update/', tag_update, name='tag_update'),
 		path('tag/<slug:slug>/delete/', tag_delete, name='tag_delete'),
 
