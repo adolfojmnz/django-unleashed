6708b160182c848429f6132ca3414093c397e288
Eadwulf <eadwulfts@protonmail.com>
2021-12-15 20:03:45 -0400

    Add Class Based View for Tag and Startup Models

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,21 +1,30 @@
 from django.urls import path
 
 from .views import (
-        homepage,
-		tag_list, tag_detail, tag_create, tag_update, tag_delete,
+		homepage,
+		TagCreate, TagUpdate, TagDelete,
+		tag_list, tag_detail,
+		StartupCreate, StartupUpdate, StartupDelete,
 		startup_list, startup_detail,
 )
 
 
 urlpatterns = [
-        # tags urls
-        path('tag/', tag_list, name='tag_list'),
-		path('tag/create/', tag_create, name='tag_create'),
-        path('tag/<slug:slug>/', tag_detail, name='tag_detail'),
-		path('tag/<slug:slug>/update/', tag_update, name='tag_update'),
-		path('tag/<slug:slug>/delete/', tag_delete, name='tag_delete'),
+		# CBV for Tag model
+		path('tag/create/', TagCreate.as_view(), name='tag_create'),
+		path('tag/<slug:slug>/update/', TagUpdate.as_view(), name='tag_update'),
+		path('tag/<slug:slug>/delete/', TagDelete.as_view(), name='tag_delete'),
 
-        # startups urls
+		# CBV for Startup class model
+        path('startup/create/', StartupCreate.as_view(), name='startup_create'),
+        path('startup/<slug:slug>/update/', StartupUpdate.as_view(), name='startup_update'),
+        path('startup/<slug:slug>/delete/', StartupDelete.as_view(), name='startup_delete'),
+
+        # Function Views for Tag model
+        path('tag/', tag_list, name='tag_list'),
+        path('tag/<slug:slug>/', tag_detail, name='tag_detail'),
+
+        # Function Views for Startup model
         path('startup/', startup_list, name='startup_list'),
         path('startup/<slug:slug>/', startup_detail, name='startup_detail'),
 ]
--- organizer/views.py
+++ organizer/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.views.generic import View
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -10,7 +11,7 @@ def homepage(request):
     template_name = 'organizer/homepage.html'
     return render(request, template_name)
 
-	
+
 def tag_list(request):
     template_name = 'organizer/tag_list.html'
     context       = {'tag_list': Tag.objects.all()}
@@ -18,12 +19,75 @@ def tag_list(request):
 
 
 def tag_detail(request, slug):
-	if slug == 'create':
-		return tag_create(request)
-	else:
-	    template_name = 'organizer/tag_detail.html'
-	    context = {'tag': get_object_or_404(Tag, slug__iexact=slug)}
-	    return render(request, template_name, context)
+    if slug == 'create':
+        return tag_create(request)
+    else:
+        template_name = 'organizer/tag_detail.html'
+        context = {'tag': get_object_or_404(Tag, slug__iexact=slug)}
+        return render(request, template_name, context)
+
+
+class TagCreate(View):
+    form_class = TagForm
+    template_name = 'organizer/tag_form_create.html'
+
+    def get(self, request):
+        context = {'form': self.form_class()}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_tag = form.save()
+            return redirect(new_tag)
+        else:
+            context ={'form': form}
+        return render(request, self.template_name, context)
+
+
+class TagUpdate(View):
+    form_class = TagForm
+    template_name = 'organizer/tag_form_update.html'
+
+    def get(self, request, slug):
+        context = {
+            'tag': Tag.objects.get(slug__iexact=slug),
+            'form': self.form_class()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, slug):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            updated_tag = form.save()
+            return redirect(updated_tag)
+        else:
+            context = {
+                'tag': Tag.objects.get(slug__iexact=slug),
+                'form': form
+            }
+        return render(request, self.template_name, context)
+
+
+class TagDelete(View):
+    form_class = TagForm
+    template_name = 'organizer/tag_form_delete.html'
+
+    def get(self, request, slug):
+        context = {
+            'tag': Tag.objects.get(slug__iexact=slug),
+            'form': self.form_class()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, slug):
+        try:
+            tag = Tag.objects.get(slug__iexact=slug)
+        except Tag.DoesNotExist:
+            raise Http404(f'Tag For {slug} Was Not Found!')
+        if tag:
+            tag.delete()
+            return redirect(tag_list)
 
 
 def startup_list(request):
@@ -38,47 +102,71 @@ def startup_detail(request, slug):
     return render(request, template_name, context)
 
 
-# helper functions
-def tag_create(request):
-	if request.method == 'POST':
-		form = TagForm(request.POST)
-		if form.is_valid():
-			new_tag = form.save()
-			return redirect(new_tag)
-		else:
-			context = {'form': form}
-	elif request.method == 'GET':
-		context = {'form': TagForm()}
+class StartupCreate(View):
+    form_class = StartupForm
+    template_name = 'organizer/startup_form_create.html'
 
-	template_name = 'organizer/tag_form_create.html'
-	return render(request, template_name, context)
+    def get(self, request):
+        context = {'form': self.form_class()}
+        return render(request, self.template_name, context)
 
-
-def tag_update(request, slug):
-	if request.method == 'POST':
-		form = TagForm(request.POST)
-		if form.is_valid():
-			updated_tag = form.save()
-			return redirect(updated_tag)
-		else:
-			context = {'slug': slug, 'form': form}
-	elif request.method == 'GET':
-		form = TagForm(instance=Tag.objects.get(slug__iexact=slug))
-		context = {'slug': slug, 'form': form}
-
-	template_name = 'organizer/tag_form_update.html'
-	return render(request, template_name, context)
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_startup = form.save()
+            return redirect(new_startup)
+        else:
+            context = {'form': form}
+        return render(request, self.template_name, context)
 
 
-def tag_delete(request, slug):
-	if request.method == 'POST':
-		tag = Tag.objects.get(slug__iexact=slug)
-		if tag:
-			tag.delete()
-			return redirect(tag_list)
-		else:
-			raise Tag.DoesNotExist
-	elif request.method == 'GET':
-		template_name = 'organizer/tag_form_delete.html'
-		context = {'tag': Tag.objects.get(slug__iexact=slug)}
-		return render(request, template_name, context)
+class StartupUpdate(View):
+    form_class = StartupForm
+    template_name = 'organizer/startup_form_update.html'
+
+    def get(self, request, slug):
+        try:
+            startup = Startup.objects.get(slug__iexact=slug)
+        except Startup.DoesNotExist:
+            return StartupCreate.get(request, slug)
+        context = {
+            'startup': Startup.objects.get(slug__iexact=slug),
+            'form': self.form_class()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, slug):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            updated_startup = form.save()
+            return redirect(updated_startup)
+        else:
+            context = {
+                'startup': Startup.objects.get(slug__iexact=slug),
+                'form': form
+            }
+        return render(request, template_name, context)
+
+
+class StartupDelete(View):
+    form_class = StartupForm
+    template_name = 'organizer/startup_form_delete.html'
+
+    def get(self, request, slug):
+        context = {
+            'startup': Startup.objects.get(slug__iexact=slug),
+            'form': self.form_class()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, slug):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.delete()
+            return redirect(startup_list)
+        else:
+            context = {
+                'startup': Startup.objects.get(slug__iexact=slug),
+                'form': form
+            }
+        return render(request, self.template_name, context)
