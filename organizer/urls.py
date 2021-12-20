7a6c701220a9fa814d6c6537636f6abadda80b5c
Eadwulf <eadwulfts@protonmail.com>
2021-12-19 22:13:22 -0400

    Change urls path to match new CBVs

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,34 +1,27 @@
 from django.urls import path
 
 from .views import (
-		homepage,
-		tag_list, tag_detail,
-		startup_list, startup_detail,
 		NewsLinkCreate,
-		TagCreate, TagUpdate, TagDelete,
-		StartupCreate, StartupUpdate, StartupDelete,
+		TagList, TagDetail, TagCreate, TagUpdate, TagDelete,
+		StartupList, StartupDetail, StartupCreate, StartupUpdate, StartupDelete,
 )
 
 
 urlpatterns = [
 		# CBV for Tag model
+        path('tag/', TagList.as_view(), name='tag_list'),
 		path('tag/create/', TagCreate.as_view(), name='tag_create'),
+        path('tag/<slug:slug>/', TagDetail.as_view(), name='tag_detail'),
 		path('tag/<slug:slug>/update/', TagUpdate.as_view(), name='tag_update'),
 		path('tag/<slug:slug>/delete/', TagDelete.as_view(), name='tag_delete'),
 
 		# CBV for Startup class model
+        path('startup/', StartupList.as_view(), name='startup_list'),
         path('startup/create/', StartupCreate.as_view(), name='startup_create'),
+        path('startup/<slug:slug>/', StartupDetail.as_view(), name='startup_detail'),
         path('startup/<slug:slug>/update/', StartupUpdate.as_view(), name='startup_update'),
         path('startup/<slug:slug>/delete/', StartupDelete.as_view(), name='startup_delete'),
 
 		# CBV for NewsLink model
 		path('newslink/create/', NewsLinkCreate.as_view(), name='newslink_create'),
-
-        # Function Views for Tag model
-        path('tag/', tag_list, name='tag_list'),
-        path('tag/<slug:slug>/', tag_detail, name='tag_detail'),
-
-        # Function Views for Startup model
-        path('startup/', startup_list, name='startup_list'),
-        path('startup/<slug:slug>/', startup_detail, name='startup_detail'),
 ]
