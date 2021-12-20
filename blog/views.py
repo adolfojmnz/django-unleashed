80ae4af118a57bd77644c34571fdccbe4b685597
Eadwulf <eadwulfts@protonmail.com>
2021-12-19 22:08:42 -0400

    Delete methods in PostDetail and PostList and add inheritance to each one of then from ObjectDetailMixin and ObjectListMixin respectively

--- blog/views.py
+++ blog/views.py
@@ -6,59 +6,40 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
-from .utils import CreateObjectMixin, UpdateObjectMixin, DeleteObjectMixin
+from .utils import (
+	ObjectListMixin, ObjectDetailMixin,
+	CreateObjectMixin, UpdateObjectMixin, DeleteObjectMixin,
+)
 
 
-class GetPostObjectMixin:
-    def get_post_object(self, year, month, slug):
-        return (
-            get_object_or_404(
-                Post,
-                pub_date__year  = year,
-                pub_date__month = month,
-                slug__iexact    = slug
-            )
-        )
+class PostList(View, ObjectListMixin):
+	model = Post
+	context_name = 'post_list'
+	template_name = 'blog/post_list.html'
 
 
-class PostList(View):
-    template_name = 'blog/post_list.html'
-
-    def get(self, request, parent_template=None):
-        context = {
-            'post_list': Post.objects.all(),
-            'parent_template': parent_template,
-        }
-        return render(request, self.template_name, context)
-
-
-class PostDetail(View, GetPostObjectMixin):
-    template_name = 'blog/post_detail.html'
-
-    def get(self, request, year, month, slug, parent_template=None):
-        context = {
-            'post': self.get_post_object(year, month, slug),
-            'parent_template': parent_template,
-        }
-        return render(request, self.template_name, context)
+class PostDetail(View, ObjectDetailMixin):
+	model = Post
+	context_name = 'post'
+	template_name = 'blog/post_detail.html'
 
 
 class PostCreate(View, CreateObjectMixin):
     form_class    = PostForm
     model         = Post
-    model_name    = 'post'
+    context_name    = 'post'
     template_name = 'blog/post_form_create.html'
 
 
 class PostUpdate(View, UpdateObjectMixin):
     form_class    = PostForm
     model         = Post
-    model_name    = 'post'
+    context_name    = 'post'
     template_name = 'blog/post_form_update.html'
 
 
 class PostDelete(View, DeleteObjectMixin):
     form_class    = PostForm
     model         = Post
-    model_name    = 'post'
+    context_name    = 'post'
     template_name = 'blog/post_form_delete.html'
