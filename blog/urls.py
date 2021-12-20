87b372791ed1eefb9b3e53fee53a7997e899b6d3
Eadwulf <eadwulfts@protonmail.com>
2021-12-19 20:31:12 -0400

    Add urls path for Create, Update and Delete classes

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 
 urlpatterns = [
-        path('blog/', PostList.as_view(), {'parent_template': 'base.html'},
-            name = 'post_list'
-		),
+	path('blog/create/', PostCreate.as_view(), name='post_create'),
+	path('blog/<int:year>/<int:month>/<slug:slug>/update/', PostUpdate.as_view(), name='post_update'),
+	path('blog/<int:year>/<int:month>/<slug:slug>/delete/', PostDelete.as_view(), name='post_delete'),
 
-        path('blog/<int:year>/<int:month>/<slug:slug>', PostDetail.as_view(), {'parent_template': 'base.html'},
-            name = 'post_detail'
-        ),
+    path('blog/', PostList.as_view(), {'parent_template': 'base.html'}, name='post_list'),
+    path('blog/<int:year>/<int:month>/<slug:slug>/', PostDetail.as_view(), {'parent_template': 'base.html'}, name='post_detail'),
 ]
