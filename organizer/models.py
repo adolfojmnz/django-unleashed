cb1bb82bde01dc63eeb5619f73dce64e793157ea
Eadwulf <eadwulfts@protonmail.com>
2021-12-24 19:27:04 -0400

    Delete kwargs in NewsLink get_url methods so it works with slug, add <slug:slug> in NewsLink's urls path and add blank=True in Startup.tags

--- organizer/models.py
+++ organizer/models.py
@@ -3,84 +3,92 @@ from django.db import models
 
 
 class GetReversedURLForSlugMixin:
-    def get_reversed_url(self, url_path_name):
-        return reverse(
-            url_path_name,
-            kwargs={'slug': self.slug}
-        )
+	def get_reversed_url(self, url_path_name):
+		return reverse(
+			url_path_name,
+			kwargs={'slug': self.slug}
+		)
 
 
 class Tag(models.Model, GetReversedURLForSlugMixin):
-    name = models.CharField(max_length=31, unique=True)
-    slug = models.SlugField(max_length=31, unique=True,
-                   help_text='A label for URL config.')
+	name = models.CharField(max_length=31, unique=True)
+	slug = models.SlugField(
+		max_length=31,
+		unique=True,
+		help_text='A label for URL config.',
+	)
 
-    class Meta:
-        ordering = ['name']
+	class Meta:
+		ordering = ['name']
 
-    def get_absolute_url(self):
-        return self.get_reversed_url(url_path_name='tag_detail')
+	def get_absolute_url(self):
+		return self.get_reversed_url('tag_detail')
 
-    def get_create_url(self):
-        return self.get_reversed_url(url_path_name='tag_create')
+	def get_create_url(self):
+		return self.get_reversed_url('tag_create')
 
-    def get_update_url(self):
-        return self.get_reversed_url(url_path_name='tag_update')
+	def get_update_url(self):
+		return self.get_reversed_url('tag_update')
 
-    def get_delete_url(self):
-        return self.get_reversed_url(url_path_name='tag_delete')
+	def get_delete_url(self):
+		return self.get_reversed_url('tag_delete')
 
-    def __str__(self):
-        return self.name.title()
+	def __str__(self):
+		return self.name.title()
 
 
 class Startup(models.Model, GetReversedURLForSlugMixin):
-    name         = models.CharField(max_length=31)
-    slug         = models.SlugField()
-    description  = models.TextField()
-    founded_date = models.DateField(help_text='Date format: DD/MM/YYYY')
-    contact      = models.EmailField(max_length=254)
-    website      = models.URLField()
-    tags         = models.ManyToManyField(Tag)
+	name         = models.CharField(max_length=31)
+	slug         = models.SlugField()
+	description  = models.TextField()
+	founded_date = models.DateField(help_text='Date format: DD/MM/YYYY')
+	contact      = models.EmailField(max_length=254)
+	website      = models.URLField()
+	tags         = models.ManyToManyField(Tag, blank=True)
 
-    class Meta:
-        ordering      = ['name']
-        get_latest_by = 'founded_date'
+	class Meta:
+		ordering      = ['name']
+		get_latest_by = 'founded_date'
 
-    def get_absolute_url(self):
-        return self.get_reversed_url(url_path_name='startup_detail')
+	def get_absolute_url(self):
+		return self.get_reversed_url('startup_detail')
 
-    def get_create_url(self):
-        return self.get_reversed_url(url_path_name='startup_create')
+	def get_create_url(self):
+		return self.get_reversed_url('startup_create')
 
-    def get_update_url(self):
-        return self.get_reversed_url(url_path_name='startup_update')
+	def get_update_url(self):
+		return self.get_reversed_url('startup_update')
 
-    def get_delete_url(self):
-        return self.get_reversed_url(url_path_name='startup_delete')
+	def get_delete_url(self):
+		return self.get_reversed_url('startup_delete')
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-class NewsLink(models.Model):
-    title        = models.CharField(max_length=63)
-    pub_date     = models.DateField('publication date')
-    link         = models.URLField(max_length=255)
-    startup      = models.ForeignKey(Startup, on_delete=models.CASCADE)
+class NewsLink(models.Model, GetReversedURLForSlugMixin):
+	title        = models.CharField(max_length=63)
+	slug         = models.SlugField(max_length=63)
+	pub_date     = models.DateField('publication date')
+	link         = models.URLField(max_length=255)
+	startup      = models.ForeignKey(Startup, on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name  = 'news article'
-        ordering      = ['-pub_date']
-        get_latest_by = 'pub_date'
+	class Meta:
+		verbose_name  = 'news article'
+		ordering      = ['-pub_date']
+		get_latest_by = 'pub_date'
+		unique_together = ('slug', 'startup')
 
-    def get_absolute_url(self):
-        return reverse('newslink_detail', kwargs={'pk': self.pk})
+	def get_absolute_url(self):
+		return self.get_reversed_url('newslink_detail')
 
-    def get_update_url(self):
-        return reverse('newslink_update', kwargs={'pk': self.pk})
+	def get_create_url(self):
+		return self.get_reversed_url('newslink_create')
 
-    def get_delete_url(self):
-        return reverse('newslink_delete', kwargs={'pk': self.pk})
+	def get_update_url(self):
+		return self.get_reversed_url('newslink_update')
 
-    def __str__(self):
-        return f'{self.title} >> {self.startup}'
+	def get_delete_url(self):
+		return self.get_reversed_url('newslink_delete')
+
+	def __str__(self):
+		return f'{self.title} >> {self.startup}'
--- organizer/urls.py
+++ organizer/urls.py
@@ -9,22 +9,22 @@ from .views import (
 
 urlpatterns = [
 		# CBV for Tag model
-        path('tag/', TagList.as_view(), name='tag_list'),
+		path('tag/', TagList.as_view(), name='tag_list'),
 		path('tag/create/', TagCreate.as_view(), name='tag_create'),
-        path('tag/<slug:slug>/', TagDetail.as_view(), name='tag_detail'),
+		path('tag/<slug:slug>/', TagDetail.as_view(), name='tag_detail'),
 		path('tag/<slug:slug>/update/', TagUpdate.as_view(), name='tag_update'),
 		path('tag/<slug:slug>/delete/', TagDelete.as_view(), name='tag_delete'),
 
 		# CBV for Startup class model
-        path('startup/', StartupList.as_view(), name='startup_list'),
-        path('startup/create/', StartupCreate.as_view(), name='startup_create'),
-        path('startup/<slug:slug>/', StartupDetail.as_view(), name='startup_detail'),
-        path('startup/<slug:slug>/update/', StartupUpdate.as_view(), name='startup_update'),
-        path('startup/<slug:slug>/delete/', StartupDelete.as_view(), name='startup_delete'),
+		path('startup/', StartupList.as_view(), name='startup_list'),
+		path('startup/create/', StartupCreate.as_view(), name='startup_create'),
+		path('startup/<slug:slug>/', StartupDetail.as_view(), name='startup_detail'),
+		path('startup/<slug:slug>/update/', StartupUpdate.as_view(), name='startup_update'),
+		path('startup/<slug:slug>/delete/', StartupDelete.as_view(), name='startup_delete'),
 
 		# CBV for NewsLink model
 		path('newslink/create/', NewsLinkCreate.as_view(), name='newslink_create'),
-		path('newslink/<int:pk>/', NewsLinkDetail.as_view(), name='newslink_detail'),
-		path('newslink/<int:pk>/update/', NewsLinkUpdate.as_view(), name='newslink_update'),
-		path('newslink/<int:pk>/delete/', NewsLinkDelete.as_view(), name='newslink_delete'),
+		path('newslink/<slug:slug>/', NewsLinkDetail.as_view(), name='newslink_detail'),
+		path('newslink/<slug:slug>/update/', NewsLinkUpdate.as_view(), name='newslink_update'),
+		path('newslink/<slug:slug>/delete/', NewsLinkDelete.as_view(), name='newslink_delete'),
 ]
