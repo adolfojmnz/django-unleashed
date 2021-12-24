03dfcb239a4972b8aee8d34f843760b236d20b70
Eadwulf <eadwulfts@protonmail.com>
2021-12-24 19:19:37 -0400

    Create GetReversedURLForSlugMixin class, add blank=True in slug and startup attributes plus minor changes

--- blog/models.py
+++ blog/models.py
@@ -3,43 +3,54 @@ from django.urls import reverse
 from organizer.models import Tag, Startup
 
 
-class Post(models.Model):
-    title    = models.CharField(max_length=63)
-    slug     = models.SlugField(
-                    max_length=63,
-                    help_text='A label for URL config',
-                    unique_for_month='pub_date')
-    text     = models.TextField()
-    pub_date = models.DateField('Published', auto_now_add=True)
-    tags     = models.ManyToManyField(Tag, related_name='blog_posts')
-    startups = models.ManyToManyField(Startup, related_name='blog_posts')
+class GetReversedURLForSlugMixin:
 
-    class Meta:
-        verbose_name  = 'blog post'
-        ordering      = ['-pub_date', 'title']
-        get_latest_by = 'pub_date'
+	def get_reversed_url(self, path_name):
+		return reverse(
+			path_name,
+			kwargs = {
+				'year': self.pub_date.year,
+				'month': self.pub_date.month,
+				'slug': self.slug
+			}
+		)
 
-    def get_reversed_url(self, path_name):
-        return reverse(
-            path_name,
-            kwargs = {
-                'year': self.pub_date.year,
-                'month': self.pub_date.month,
-                'slug': self.slug
-            }
-        )
 
-    def get_absolute_url(self):
-        return self.get_reversed_url(path_name='post_detail')
+class Post(models.Model, GetReversedURLForSlugMixin):
+	title    = models.CharField(max_length=63)
+	slug     = models.SlugField(
+					max_length=63,
+					help_text='A label for URL config',
+					unique_for_month='pub_date')
+	text     = models.TextField()
+	pub_date = models.DateField('Published', auto_now_add=True)
+	tags     = models.ManyToManyField(
+		Tag,
+		blank=True,
+		related_name='blog_posts',
+	)
+	startups = models.ManyToManyField(
+		Startup,
+		blank=True,
+		related_name='blog_posts',
+	)
 
-    def get_create_url(self):
-        return self.get_reversed_url(path_name='post_create')
+	class Meta:
+		verbose_name  = 'blog post'
+		ordering      = ['-pub_date', 'title']
+		get_latest_by = 'pub_date'
 
-    def get_update_url(self):
-        return self.get_reversed_url(path_name='post_update')
+	def get_absolute_url(self):
+		return self.get_reversed_url('post_detail')
 
-    def get_delete_url(self):
-        return self.get_reversed_url(path_name='post_delete')
+	def get_create_url(self):
+		return self.get_reversed_url('post_create')
 
-    def __str__(self):
-        return f'{self.title} on {self.pub_date.strftime("%Y-%m-%d")}'
+	def get_update_url(self):
+		return self.get_reversed_url('post_update')
+
+	def get_delete_url(self):
+		return self.get_reversed_url('post_delete')
+
+	def __str__(self):
+		return f'{self.title} on {self.pub_date.strftime("%Y-%m-%d")}'
